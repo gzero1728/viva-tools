55e7d2c93ec89d797110ab0ac40af56b3c36c58e
gzero1728 <gzero1728@gmail.com>
2025-02-16 23:10:51 +0900

    [FIX] : vite config 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
     plugins: [react(), tsconfigPaths()],
+    base: "/viva-tools/",
     server: {
       port: 3000,
     },
