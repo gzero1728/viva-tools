eead4fc76e4ff80a614ccfa0121f52c19bf02bc9
gzero1728 <gzero1728@gmail.com>
2025-02-16 23:00:43 +0900

    [FIX] :vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,15 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(() => {
   return {
     plugins: [react(), tsconfigPaths()],
-    base: "/viva-tools/",
     server: {
       port: 3000,
-      proxy: {
-        "/api": {
-          target: "http://localhost:8000",
-          changeOrigin: true,
-        },
-      },
     },
     build: {
       outDir: "build",
