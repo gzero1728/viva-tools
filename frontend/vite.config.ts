9739c70dad710a3c8ef32d8acf6f5008da8ae93b
gzero1728 <gzero1728@gmail.com>
2025-02-16 20:38:50 +0900

    [FEATURE] : 백엔드, 프론트엔드 배포 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "/viva-tools/",
   server: {
     port: 3000,
     proxy: {
       "/api": {
         target: "http://localhost:8000",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
